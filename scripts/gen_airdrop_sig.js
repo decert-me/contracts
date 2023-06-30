a0e1bace434349cb7e4c6b04d1915ffc9cb79ca4
梁文杰 <1139629972@qq.com>
2023-06-30 10:15:33 +0800

    del: 删除默认私钥

--- scripts/gen_airdrop_sig.js
+++ scripts/gen_airdrop_sig.js
@@ -4,7 +4,7 @@ const BadgeAddr = (require(`../deployments/${network.name}/Badge.json`)).address
 async function main() {
     let tokenIdList = [10309,10308,10308]   // 需要空投的token id
     let sender = "0x7d32D1DE76acd73d58fc76542212e86ea63817d8" // 空投发起地址
-    let signerPrivateKey = "0x0000000ec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";  // 签名者私钥
+    let signerPrivateKey = "";  // 签名者私钥
     let badgeAddr = BadgeAddr    // Badge 合约地址
 
     const signer = new ethers.Wallet(signerPrivateKey);
